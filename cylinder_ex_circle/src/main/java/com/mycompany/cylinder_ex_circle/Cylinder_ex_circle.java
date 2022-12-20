47475890f23633e32b47bb181f84a1cd5d939588
tokaashawky <119989493+tokaashawky@users.noreply.github.com>
2022-12-20 21:36:27 +0200

    Delete cylinder_ex_circle directory

--- cylinder_ex_circle/src/main/java/com/mycompany/cylinder_ex_circle/Cylinder_ex_circle.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.cylinder_ex_circle;
-public class Cylinder_ex_circle {
-    public static void main(String[] args) {
-        circle c1=new circle(20,"blue");
-        cylinder c2=new cylinder(5,20,"red");
-        System.out.println(c1.toString()+"-->Area= "+c1.getArea());
-        System.out.println(c2.toString()+"-->Volume= "+c2.getvolume());
-    }
-}
